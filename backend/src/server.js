64bb6f0a48ae5858bc8b5b766b9efb1b3eecff74
Pravin <pravinpagar290@gmail.com>
2026-02-18 22:54:41 +0530

    feat: set up initial backend infrastructure including Express server, Mongoose User model, and environment variable configuration.

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,16 @@ import express from "express";
 import { ENV } from "./utils/env.js";
 import path from "path";
 import { connectDB } from "./utils/db.js";
+import cors from "cors";
 
 const app = express();
+app.use(
+  cors({
+    origin: ENV.CLIENT_URL,
+    credentials: true,
+  }),
+);
 const __dirname = path.resolve();
-
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
@@ -14,23 +20,20 @@ app.get("/health", (req, res) => {
 // make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  
+
   app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-
-const serverStart=async()=>{
+const serverStart = async () => {
   try {
-    await connectDB()
+    await connectDB();
     app.listen(ENV.PORT, () => {
       console.log(`Server is running on PORT ${ENV.PORT}`);
     });
-    
   } catch (error) {
     console.error("unable to start the server");
-    
   }
-}
-serverStart()
+};
+serverStart();
