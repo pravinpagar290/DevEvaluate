35cb412c8234ee681ae9342fff64cb0ca16e682f
Pravin <pravinpagar290@gmail.com>
2026-02-23 23:10:48 +0530

    server side auth implemented

---
+++ backend/src/controllers/chat.controller.js
@@ -0,0 +1,19 @@
+import {chatClient} from "../lib/stream.js"
+
+export async function getStreamToken(req,res) {
+    try {
+        const token = chatClient.createToken(req.user.clerkId)
+
+        res.status(200).json({
+            token,
+            userId:req.user.clerkId,
+            userName:req.user.name,
+            userImage:req.user.image,
+        });
+    } catch (error) {
+        console.error("Error in getStream controller:",error.message);
+        res.status(500).json({
+            message:"Internal server error"
+        });
+    }
+}
